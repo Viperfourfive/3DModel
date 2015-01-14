84b7e7ff7a497d5e0595719c307da0c77e64dcad
Viperfourfive <viperfourfive@hotmail.com>
2015-01-14 02:36:21 -0700

    First

---
+++ 3DModel/Game1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace _3DModel
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Model model;
+
+        private Matrix world = Matrix.CreateTranslation(0,0,0);
+        private Matrix view = Matrix.CreateLookAt(new Vector3(-4, 3, 5), new Vector3(2, -1, 0), Vector3.Up);
+        private Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(60), 800f / 480f, 0.1f, 100f);
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            model = Content.Load<Model>("1x2x5");
+
+            // TODO: use this.Content to load your game content here
+        }
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            DrawModel(model, world, view, projection);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+
+                mesh.Draw();
+            }
+        }
+    }
+}
